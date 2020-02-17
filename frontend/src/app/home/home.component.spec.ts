48308bf3a365829aa18ae9f5efaa235c79333140
Xavier Martinez <blackshieldsx@gmail.com>
2020-02-17 07:19:28 -0800

    Creating and setting up home Folder

---
+++ frontend/src/app/home/home.component.spec.ts
@@ -0,0 +1,29 @@
+/** ****************************************************************************
+ * login.component.spec.ts Copyright ©️ 2020 by the HabitRabbit developers (ardianq, lachchri16, sweiland, YellowIcicle).
+ ******************************************************************************/
+
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {HomeComponent} from './login.component';
+
+describe('LoginComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [HomeComponent]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

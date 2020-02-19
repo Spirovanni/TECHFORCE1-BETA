c7dedb0139115d9e3080af1477c923c58e30fdbc
Xavier Martinez <blackshieldsx@gmail.com>
2020-02-18 20:29:07 -0800

    import { PagesComponent } from './pages/pages.component';

--- frontend/src/app/app-routing.ts
+++ frontend/src/app/app-routing.ts
@@ -1,2 +1,4 @@
 import { Routes, RouterModule, PreloadAllModules  } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
+
+import { PagesComponent } from './pages/pages.component';

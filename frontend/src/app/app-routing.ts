8c1a9a3c67c85a54f4cfcb6988c23cfd563e212c
Xavier Martinez <blackshieldsx@gmail.com>
2020-02-18 20:31:59 -0800

    import { BlankComponent } from './pages/blank/blank.component';

--- frontend/src/app/app-routing.ts
+++ frontend/src/app/app-routing.ts
@@ -2,3 +2,4 @@ import { Routes, RouterModule, PreloadAllModules  } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
 
 import { PagesComponent } from './pages/pages.component';
+import { BlankComponent } from './pages/blank/blank.component';

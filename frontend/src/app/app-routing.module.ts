a9d6ad14d9d4fa0107dafe40a798c1789cf45831
Xavier Martinez <blackshieldsx@gmail.com>
2020-05-09 17:51:56 -0700

    Connected habit-dashboard to router

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -64,6 +64,7 @@ const routes: Routes = [
     path: 'pages',
     component: PagesComponent, children: [
       { path: '', loadChildren: () => import('./pages/admin/admin.module').then(m => m.AdminModule), data: { breadcrumb: 'Admin' }},
+      { path: 'habit-dashboard', loadChildren: () => import('./pages/habit-dashboard/habit-dashboard.module').then(m => m.HabitDashboardModule), data: { breadcrumb: 'Habit Dashboard' } },
       { path: 'users', loadChildren: () => import('./pages/users/users.module').then(m => m.UsersModule), data: { breadcrumb: 'Users' } },
       { path: 'ui', loadChildren: () => import('./pages/ui/ui.module').then(m => m.UiModule), data: { breadcrumb: 'UI' } },
       { path: 'dynamic-menu', loadChildren: () => import('./pages/dynamic-menu/dynamic-menu.module').then(m => m.DynamicMenuModule), data: { breadcrumb: 'Dynamic Menu' }  },

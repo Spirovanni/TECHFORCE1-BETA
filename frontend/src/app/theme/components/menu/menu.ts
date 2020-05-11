9cf0cceb38feb72a1a770f6a1ca69afca1ccf4c8
Xavier Martinez <blackshieldsx@gmail.com>
2020-05-10 17:19:57 -0700

    Put proper header in habit -list

--- frontend/src/app/theme/components/menu/menu.ts
+++ frontend/src/app/theme/components/menu/menu.ts
@@ -2,9 +2,9 @@ import { Menu } from './menu.model';
 
 export const verticalMenuItems = [
     new Menu (1, 'Dashboard', '/pages', null, 'dashboard', null, false, 0),
-    new Menu (2, 'Habit Dashboard', '/pages/habit-dashboard', null, 'folder', null, false, 0),
+    new Menu (2, 'Habit Dashboard', '/pages/habit-dashboard/barbicon', null, 'folder', null, false, 0),
     new Menu (3, 'Habits', null, null, 'check_box', null, true, 0),
-    new Menu (4, 'Habit List', '/pages/admin', null, 'view_module', null, false, 3),
+    new Menu (4, 'Habit List', '/pages/habit-list/habit-list', null, 'view_list', null, false, 3),
     new Menu (5, 'New Habit', '/pages/admin', null, 'view_module', null, false, 3),
     new Menu (6, 'Users', null, null, 'account_balance_wallet', null, true, 0),
     new Menu (7, 'User List', '/pages/admin', null, 'assignment_ind', null, false, 6),

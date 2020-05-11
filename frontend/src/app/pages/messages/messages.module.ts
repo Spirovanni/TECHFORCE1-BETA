afe077fc1ad6f48e52b1e42c20bb300c6ce9e98b
Xavier Martinez <blackshieldsx@gmail.com>
2020-05-11 09:01:36 -0700

    Working on the messages.module to get it to update

--- frontend/src/app/pages/messages/messages.module.ts
+++ frontend/src/app/pages/messages/messages.module.ts
@@ -4,7 +4,7 @@ import {RouterModule, Routes} from '@angular/router';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { SharedModule } from '../../shared/shared.module';
 import { DashMessageListComponent } from './message-list/dash-message-list.component';
-import { DashMessageFormComponent } from './message-form/message-form.component';
+import { DashMessageFormComponent } from './message-form/dash-message-form.component';
 import {AuthGuard} from '../../auth.guard';
 import {UserResolver} from '../../resolver/user.resolver';
 import {HabitResolver} from '../../resolver/habit.resolver';
@@ -31,7 +31,7 @@ export const routes = [
   },
   {
     path: 'message-form',
-    component: DashMessageListComponent,
+    component: DashMessageFormComponent,
     data: { breadcrumb: 'Message Form' },
     canActivate: [AuthGuard],
     resolve: {habits: HabitUserResolver, typeOptions: TypesResolver, user: CurrentUserResolver, users: UsersResolver},
